b408b508539c43f0c38f353c7ff983b7da695b77
dawnrose73 <anne.g.713@gmail.com>
2021-09-29 20:07:30 +0300

    Fix elements size

--- src/index.js
+++ src/index.js
@@ -24,78 +24,50 @@ const mySwiperLive = new Swiper('.live-swiper-container', {
         prevEl: '.live-button-prev',
       },
 });
-// document.addEventListener('DOMContentLoaded', () => {
 
-    const videobox = document.querySelector('.home__videobox');
-    videobox.classList.add('home__videobox--animated');
 
-    const addActive = (item) => {
-        item.classList.add('header__menu-link--active');
+const videobox = document.querySelector('.home__videobox');
+videobox.classList.add('home__videobox--animated');
+
+const addActive = (item) => {
+    item.classList.add('header__menu-link--active');
+}
+
+const removeActive = (item) => {
+    item.classList.remove('header__menu-link--active');
+}
+
+const activeLinks = () => {
+    const menuLinks = document.querySelectorAll('.header__menu-link');
+    const [homeLink, musicLink, videoLink] = menuLinks;
+
+    const home = document.querySelector('#home');
+    const music = document.querySelector('#music');
+    const video = document.querySelector('#video');
+
+    let windowBottom = (window.innerHeight * 3 / 4) + window.scrollY;
+
+    if ((windowBottom >= home.offsetTop) && (windowBottom < music.offsetTop)) {
+        menuLinks.forEach((el) => {
+            (el === homeLink) ? addActive(el) : removeActive(el);
+        })
     }
 
-    const removeActive = (item) => {
-        item.classList.remove('header__menu-link--active');
-    }
+    if ((windowBottom >= music.offsetTop) && (windowBottom < video.offsetTop)) {
+        menuLinks.forEach((el) => {
+            (el === musicLink) ? addActive(el) : removeActive(el);
+        })
+    } 
 
-    const activeLinks = () => {
-        const menuLinks = document.querySelectorAll('.header__menu-link');
-        const [homeLink, musicLink, videoLink] = menuLinks;
+    if (windowBottom >= video.offsetTop) {
+        menuLinks.forEach((el) => {
+            (el === videoLink) ? addActive(el) : removeActive(el);
+        })
+    } 
+}
 
-        const home = document.querySelector('#home');
-        const music = document.querySelector('#music');
-        const video = document.querySelector('#video');
-
-        let windowBottom = (window.innerHeight * 3 / 4) + window.scrollY;
-
-        if ((windowBottom >= home.offsetTop) && (windowBottom < music.offsetTop)) {
-            menuLinks.forEach((el) => {
-                (el === homeLink) ? addActive(el) : removeActive(el);
-            })
-        }
-
-        if ((windowBottom >= music.offsetTop) && (windowBottom < video.offsetTop)) {
-            menuLinks.forEach((el) => {
-                (el === musicLink) ? addActive(el) : removeActive(el);
-            })
-        } 
-
-        if (windowBottom >= video.offsetTop) {
-            menuLinks.forEach((el) => {
-                (el === videoLink) ? addActive(el) : removeActive(el);
-            })
-        } 
-    }
-
+activeLinks();
+window.addEventListener('scroll', () => {
     activeLinks();
-    window.addEventListener('scroll', () => {
-        activeLinks();
-    })
+})
 
-// })
-
-// const mySwiper = new Swiper('.official-swiper-container', {
-//     slidesPerView: 3,
-//     loop: true,
-//     navigation: {
-//         nextEl: '.official-button-next',
-//         prevEl: '.official-button-prev',
-//       },
-// });
-
-// const mySwiperDoc = new Swiper('.doc-swiper-container', {
-//     // slidesPerView: 3,
-//     // loop: true,
-//     navigation: {
-//         nextEl: '.doc-button-next',
-//         prevEl: '.doc-button-prev',
-//       },
-// });
-
-// const mySwiperLive = new Swiper('.live-swiper-container', {
-//     slidesPerView: 3,
-//     loop: true,
-//     navigation: {
-//         nextEl: '.live-button-next',
-//         prevEl: '.live-button-prev',
-//       },
-// });
